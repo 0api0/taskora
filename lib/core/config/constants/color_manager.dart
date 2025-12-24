f610944318032af4b58b99ae8e06af1a4139aafe
Mohammed <moeenharb76@gmail.com>
2025-12-24 17:24:44 +0200

    Setup color app

---
+++ lib/core/config/constants/color_manager.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  ColorManager._();
+
+  // TODO: Color primary
+  static const primaryColorApp = Color(0xff5B6CFF);
+
+  // TODO: Color background
+  // primary
+  static const backgroundPrimaryColor = Color(0xffF8FAFC);
+
+  // secondary as background for app ,  or for selected card
+  static const backgroundSecondaryColor = Color(0xffEFF1FF);
+
+  // TODO: Color text
+  // primary
+  static const textPrimaryColor = Color(0xff1A2C62);
+
+  // hint
+  static const textHintColor = Color(0xff64748B);
+
+  // secondary color text as a primary color app
+
+  // default
+  static const textDarkColor = Color(0xff64748B);
+
+  // TODO: Color Status
+  // red
+  static const statusRedColor = Color(0xffEF4444);
+
+  //  orange
+  static const statusOrangeColor = Color(0xffFB923C);
+  static const statusBackgroundOrangeColor = Color(0xffF5E2A9);
+
+  // blue
+  static const statusBlueColor = Color(0xff6380D7);
+  static const statusBackgroundBlueColor = Color(0xffE2E8F0);
+
+  // green
+  static const statusGreenColor = Color(0xff22C55E);
+  static const statusBackgroundGreenColor = Color(0xffA5F7C3);
+}
