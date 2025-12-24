49909f068858db2d87c7c10d6eee347c70cfe32c
Mohammed <moeenharb76@gmail.com>
2025-12-24 17:42:50 +0200

    Create app strings

---
+++ lib/core/config/constants/app_strings.dart
@@ -0,0 +1,17 @@
+class AppStrings {
+  AppStrings._();
+
+  //TODO: App name
+  static const String appName = 'Taskora';
+
+  //TODO: Onboarding
+  // view 1
+  static const String onboardingView1Title = 'Follow your projects with ease';
+  static const String onboardingView1SubTitle =
+      '“Manage all your projects and tasks from one place.”';
+
+  // view 2
+  static const String onboardingView2Title = 'Calculate your profits easily';
+  static const String onboardingView2SubTitle =
+      '“Record the hours worked for each task and let the app calculate the amount due automatically.”';
+}
