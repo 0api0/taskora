7325f6bc68fd199e4e06b0b64c690f7e426e7bbf
Mohammed <moeenharb76@gmail.com>
2025-12-25 14:16:25 +0200

    Create app strings part 2

--- lib/core/config/constants/app_strings.dart
+++ lib/core/config/constants/app_strings.dart
@@ -14,4 +14,162 @@ class AppStrings {
   static const String onboardingView2Title = 'Calculate your profits easily';
   static const String onboardingView2SubTitle =
       '“Record the hours worked for each task and let the app calculate the amount due automatically.”';
+
+  //TODO: button
+  // Auth
+  static const String buttonNext = 'Next';
+  static const String buttonGetStarted = 'Get Started';
+  static const String buttonLoginIn = 'Log In';
+  static const String buttonSignUp = 'Sign Up';
+  static const String buttonForgetPass = 'Forget Password ?';
+  static const String buttonVerifyEmail = 'Verify Email';
+  static const String buttonLogOut = 'Log out';
+  static const String buttonChangePassword = 'Change Password';
+  static const String buttonContinue = 'Continue';
+
+  // projects & tasks
+  static const String buttonSkipForNew = 'Skip for new';
+  static const String buttonAddProject = 'Add Project';
+  static const String buttonAddTask = 'Add Task';
+  static const String buttonCreateProject = 'Create Project ';
+  static const String buttonCreateTask = 'Create Task';
+  static const String buttonEditProject = 'Edit Project';
+  static const String buttonEditTask = 'Edit Task';
+
+  // Status
+  static const String buttonDelete = 'Delete';
+  static const String buttonCancel = 'Cancel';
+  static const String buttonSaveChanges = 'Save Changes';
+  static const String buttonRetry = 'Retry';
+  static const String buttonTryAgain = 'Try again';
+  static const String buttonGoToSetting = 'Go to setting';
+  static const String buttonContactSupport = 'Contact support';
+
+  //TODO: title features
+  static const String titleCreateAAccount = 'Create a Account';
+  static const String titleEnterYourEmailAddress = 'Enter your email address';
+  static const String titleEnterCode = 'Enter Code';
+  static const String titleGreatNewPassword = 'Great new password';
+  static const String titleProfile = 'Profile';
+  static const String titleProjects = 'Projects';
+  static const String titleProjectDetails = 'Project Details';
+  static const String titleTasks = 'Tasks';
+  static const String titleTaskDetails = 'Task Details ';
+
+  //TODO: Forms
+
+  // Auth
+  static const String formEmail = 'E-mail';
+  static const String formHintEmail = 'noorjber@gmail.com';
+  static const String formPassword = 'Password';
+  static const String formConfirmPassword = 'Confirm Password';
+  static const String formHintPassword = 'xxxxxxxxx';
+  static const String formName = 'Name';
+  static const String formHintName = 'Enter Your Name';
+  static const String formRememberMe = 'Remember me';
+  static const String formCreateAccount =
+      'Don\'t have an account? Create an account';
+  static const String formLogIn = 'Do you have an account? Log in';
+  static const String formReCode = 'Don’t receive code ?Resend code';
+
+  // Form status mess
+  static const String formEmailWrong =
+      'Please enter your email address correctly so that you can reset your password.';
+  static const String formCodeWrong = 'Wrong code . Please try again';
+  static const String formCodeCorrect = 'Correct code';
+  static const String formCodeHourlyRate =
+      'You can adjust this price for this project only without affecting the overall settings.';
+
+  // Create project
+  static const String formHourlyRate = 'Hourly Rate';
+  static const String formHintHourlyRate = '\$50';
+  static const String formProjectName = 'Project Name';
+  static const String formHintProjectName = 'e.g  Mobile App Redesign';
+  static const String formProjectDescription = 'Project Description';
+  static const String formHintProjectDescription =
+      'Add project details, requirements and objectives';
+  static const String formDeliveryDate = 'Delivery date';
+
+  // Create task
+  static const String formTaskName = 'Task Name';
+  static const String formHintTaskName = 'What need  to be done ?';
+  static const String formTaskDescription = 'Task Description';
+  static const String formHintTaskDescription =
+      'Add details ,links,or sub -tasks ';
+  static const String formPriority = 'Priority';
+  static const String formLowPriority = 'Low';
+  static const String formMediumPriority = 'Medium';
+  static const String formHighPriority = 'High';
+  static const String formEstHours = 'Est :Hours';
+  static const String formESTIMATEDHOURS = 'ESTIMATED HOURS';
+  static const String formStatus = 'Status';
+
+  //TODO: Mess
+
+  //  Confirm
+  static const String messgaesAreYouSure = 'Are You Sure ?';
+  static const String messgaesSubAreYouSure =
+      'This action cannot be undone . This Task and all its associated comments and files will be permanently removed . ';
+  static const String messgaesSubVarAreYouSure =
+      // \$ for any name , well be late
+      'This action cannot be undone . You are about to permanently delete the project “\$Website Redesign\$”  and it’s associated tasks . ';
+
+  // Error status
+  static const String messgaesNoInternetConnection = 'No internet connection';
+  static const String messgaesSubNoInternetConnection =
+      'Please check your network and try again';
+  static const String messgaesSomethingWentWrong = 'Something went wrong';
+  static const String messgaesSubSomethingWentWrong =
+      'Our servers are having trouble right now';
+  static const String messgaesLoginFailed = 'login failed';
+  static const String messgaesSubLoginFailed =
+      'Email is incorrect . Please double -check your credentials . ';
+
+  // Welcome & start & create
+  static const String messgaesWelcomeBack = 'Welcome Back ! 👋';
+  static const String messgaesSubWelcomeBack =
+      'Please login or sign up to continue our opp';
+  static const String messgaesSubCreateAccount =
+      'Create your account and enjoy the best services';
+  static const String messgaesSubReceiveCode =
+      'You will receive a code to reset your password ';
+  static const String messgaesSubCheckCode =
+      'Please check your email for the confirmation code we sent  ';
+  static const String messgaesSubGreatPassword =
+      'Your new password must be different from the previous number ';
+  static const String messgaesWelcomeProjects = 'Welcome 👋';
+  static const String messgaesSubWelcomeProjects =
+      'You haven’t added any Projects yet . Start by creating your first project . ';
+  static const String messgaesNoProjectYet = 'No project yet';
+  static const String messgaesSubNoProjectYet =
+      'Projects help you organize your work and track progress';
+  static const String messgaesNoTasksYet = 'No Tasks yet';
+  static const String messgaesSubNoTasksYetGetStart =
+      'Get Started by adding your first task to this project timeline ';
+  static const String messgaesSubNoTasksYetHere =
+      'Tasks you create will appear here';
+  static const String messgaesSetupProject =
+      'Setup a new Project to track time and expenses';
+  static const String messgaesCreateTasks = 'Let’s Start something new!👋';
+  static const String messgaesSubCreateTasks =
+      'Create your first tasks to get rolling .';
+
+  // TODO: Category
+  static const String categoryTOTALEARNINGS = 'TOTAL EARNINGS';
+  static const String categoryPROJECTS = 'PROJECTS';
+  static const String categoryCompletedTasks = 'Completed Tasks';
+  static const String categoryRemainingTasks = 'Remaining tasks';
+  static const String categoryDone = 'Done';
+  static const String categoryInProgress = 'In Progress';
+  static const String categoryToDo = 'To Do';
+  static const String categoryWORKINGHOURS = 'WORKING HOURS';
+  static const String categoryUpcomingTasks = 'Upcoming Tasks';
+  static const String categorySeeAll = 'See All';
+  static const String categoryAccountInformation = 'Account Information';
+  static const String categoryHourlyRate = 'Hourly Rate';
+  static const String categoryMemberSince = 'Member since';
+  static const String categoryTIMETRACKED = 'TIME TRACKED';
+  static const String categoryDEADLINE = 'DEADLINE';
+  static const String categoryOverview = 'Overview';
+  static const String categoryTASKSTATUS = 'TASK STATUS';
 }
