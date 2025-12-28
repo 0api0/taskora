41a8145aa666addc816fb6b9bb07604d7ff945f6
Mohammed <moeenharb76@gmail.com>
2025-12-28 17:55:59 +0200

    Add app sizes and image path

---
+++ lib/core/config/constants/app_sizes.dart
@@ -0,0 +1,58 @@
+class AppSizes {
+  AppSizes._();
+
+  static const double heightButton = 48;
+  static const double heightField = 44;
+  static const double heightPic = 267;
+
+  // send code
+  static const double heightFieldCode = 50;
+  static const double widthFieldCode = 45;
+
+  // profile
+  static const double profileCircle = 118;
+  static const double profileCircleAppBar = 45;
+
+  // Logo
+  static const double heightLogo = 177;
+  static const double widthLogo = 156;
+
+  // On boarding & status
+  static const double circleOnBoarding = 6; // w & h
+  static const double circleOnBoardingSelected = 30; // w
+  static const double circleStatus = 100;
+
+  // icons
+  static const double iconSmall = 16;
+  static const double iconNormal = 24;
+  static const double iconExtra = 54;
+
+  // text
+  static const int textTitle = 18;
+  static const int textTitleBody = 16;
+  static const int textTitleBodyExtra = 24;
+  static const int textSubTitle = 14;
+
+  // Field
+  static const int textTitleFieldDefault = 16;
+  static const int textTitleFieldSelected = 14;
+  static const int textHintField = 11;
+  static const int textRichButton = 10;
+  static const int textStatusField = 12;
+
+  // text profile
+  static const int textTitleProfile = 14;
+  static const int textSubTitleProfile = 10;
+
+  // Category
+  static const int textCategoryNormal = 16;
+  static const int textCategorySmall = 14;
+  static const int textCategoryMid = 12;
+  static const int textCategoryExtraSmall = 10;
+  static const int textCategoryExtraSmallX = 8;
+
+  // text button
+  static const int textButtonNavBar = 10;
+  static const int textButtonNormal = 16;
+  static const int textButtonSmall = 10;
+}
---
+++ lib/core/config/constants/image_path.dart
@@ -0,0 +1,8 @@
+class ImagePath {
+  ImagePath._();
+
+  static const String logo = 'assets/images/Logo.png';
+  static const String onBoarding_1 = 'assets/images/On1.png';
+  static const String onBoarding_2 = 'assets/images/On2.png';
+  static const String onBoarding_3 = 'assets/images/On3.png';
+}
