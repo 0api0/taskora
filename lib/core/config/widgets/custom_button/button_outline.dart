a457ddd6384090bac1f895329bfff4bb5aa871c9
Mohammed <moeenharb76@gmail.com>
2025-12-29 12:23:55 +0200

    Setup theme data part 2 with create buton states (ElevatedButton),and refactor text theme ,

---
+++ lib/core/config/widgets/custom_button/button_outline.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/color_manager.dart';
+
+class ButtonOutLine extends StatelessWidget {
+  const ButtonOutLine({super.key, required this.text, this.onPressed});
+
+  final String text;
+  final Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: ColorManager.backgroundPrimaryColor,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: ColorManager.primaryColorApp),
+          borderRadius: BorderRadiusGeometry.circular(4),
+        ),
+      ),
+      child: Text(text, style: TextStyle(color: ColorManager.primaryColorApp)),
+    );
+  }
+}
--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -4,62 +4,96 @@ import 'package:taskora/core/config/constants/color_manager.dart';
 class ThemeDataLight {
   ThemeDataLight._();
 
-  static const String _fontFamily = 'Cairo';
+  static const String fontFamily = 'Cairo';
 
   static final ThemeData themeData = ThemeData(
     // general
     brightness: Brightness.light,
-    fontFamily: _fontFamily,
+    fontFamily: fontFamily,
     colorScheme: ColorScheme.fromSeed(
       seedColor: ColorManager.primaryColorApp,
       brightness: Brightness.light,
     ),
     scaffoldBackgroundColor: ColorManager.backgroundPrimaryColor,
 
+    // Button
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        minimumSize: WidgetStateProperty.all(const Size(double.infinity, 48)),
+        elevation: WidgetStateProperty.all(0),
+        // background ' if disabled and if enabled '
+        backgroundColor: WidgetStateColor.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) {
+            return ColorManager.backgroundDisable;
+          }
+          return ColorManager.primaryColorApp;
+        }),
+
+        // foreground color for color text
+        foregroundColor: WidgetStateProperty.all(
+          ColorManager.backgroundPrimaryColor,
+        ),
+
+        // here no color text bec.. widget state maybe disabled or enabled...
+        //  but color in text in (ui view , not themeData) well be visible
+        textStyle: WidgetStateProperty.all(
+          const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+            fontFamily: fontFamily,
+            height: 20 / 16,
+            letterSpacing: 0,
+          ),
+        ),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        ),
+      ),
+    ),
     // Text theme
     textTheme: TextTheme(
       // extra bold
       titleLarge: TextStyle(
-        fontFamily: _fontFamily,
+        fontFamily: fontFamily,
         fontWeight: FontWeight.w800,
         fontSize: 28,
-        height: 100,
+        height: 100 / 28,
         letterSpacing: 0,
       ),
 
       // bold
       titleMedium: TextStyle(
-        fontFamily: _fontFamily,
+        fontFamily: fontFamily,
         fontWeight: FontWeight.w700,
         fontSize: 18,
-        height: 30,
+        height: 30 / 18,
         letterSpacing: 0,
       ),
 
       // Regular
       bodyLarge: TextStyle(
-        fontFamily: _fontFamily,
+        fontFamily: fontFamily,
         fontWeight: FontWeight.w400,
         fontSize: 14,
-        height: 100,
+        height: 100 / 14,
         letterSpacing: 0,
       ),
 
       // Medium
       bodyMedium: TextStyle(
-        fontFamily: _fontFamily,
+        fontFamily: fontFamily,
         fontWeight: FontWeight.w500,
         fontSize: 12,
-        height: 100,
+        height: 100 / 12,
         letterSpacing: 0,
       ),
 
       // small
       bodySmall: TextStyle(
-        fontFamily: _fontFamily,
+        fontFamily: fontFamily,
         fontWeight: FontWeight.w500,
         fontSize: 8,
-        height: 14,
+        height: 14 / 8,
         letterSpacing: 0,
       ),
     ),
