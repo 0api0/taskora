9b10778d9f43e81e13ed21c24e66d1fe937450ed
Mohammed <moeenharb76@gmail.com>
2025-12-31 12:27:59 +0200

    Add 2 image error status

--- lib/core/config/constants/image_path.dart
+++ lib/core/config/constants/image_path.dart
@@ -5,4 +5,6 @@ class ImagePath {
   static const String onBoarding_1 = 'assets/images/On1.png';
   static const String onBoarding_2 = 'assets/images/On2.png';
   static const String onBoarding_3 = 'assets/images/On3.png';
+  static const String serverError = 'assets/images/Group 38 (1).png';
+  static const String internetError = 'assets/images/no_internet.png';
 }
