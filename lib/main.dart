e3b9dfae04b2ee20fa2c6c878c05c12c13703f43
Mohammed <moeenharb76@gmail.com>
2026-01-01 11:58:17 +0200

    Add padding scaffold extension with edit custom app bar , and test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:taskora/core/config/constants/app_strings.dart';
-import 'package:taskora/core/config/constants/image_path.dart';
+import 'package:taskora/core/config/widgets/custom_app_bar.dart';
+import 'package:taskora/core/config/widgets/custom_button/custom_button.dart';
+import 'package:taskora/core/extensions/padding_extension.dart';
 import 'package:taskora/core/theme/light_theme.dart';
-import 'core/config/widgets/custom_body_error.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,15 +18,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: CustomBodyError(
-              image: ImagePath.internetError,
-              title: AppStrings.messgaesNoInternetConnection,
-              subTitle: AppStrings.messgaesSubNoInternetConnection,
-              textButton: AppStrings.buttonRetry,
-              textButtonOutline: AppStrings.buttonGoToSetting,
-              onPressed: () {},
-              onPressedOutline: () {},
+          child: Padding(
+            padding: context.paddingScaffold,
+            child: const Column(
+              children: [
+                CustomAppBar(title: 'title'),
+                CustomButton(text: 'text'),
+              ],
             ),
           ),
         ),
