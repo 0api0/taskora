0a23adaa508612968604ed28cfe0b9ade712ac83
Mohammed <moeenharb76@gmail.com>
2026-01-13 17:16:24 +0200

    Edit home test

--- lib/features/splash_onboarding/presentation/views/home_test.dart
+++ lib/features/splash_onboarding/presentation/views/home_test.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:taskora/core/extensions/text_style_extension.dart';
 
 class HomeTest extends StatelessWidget {
   const HomeTest({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('data'));
+    return Scaffold(
+      body: Center(child: Text('Home test', style: context.extraBold)),
+    );
   }
 }
