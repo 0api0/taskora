81f6cc4ba71a6d69eb127568c02b3584f6820d79
Mohammed <moeenharb76@gmail.com>
2026-03-30 13:12:44 +0300

    Edit login view body and create login view body content

--- lib/features/auth/presentation/widgets/login_view_widgets/login_view_body.dart
+++ lib/features/auth/presentation/widgets/login_view_widgets/login_view_body.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:taskora/core/extensions/sizes_extension.dart';
 import 'package:taskora/core/router/routers_name.dart';
-import 'login_footer_section.dart';
-import 'login_form_section.dart';
-import 'login_header_section.dart';
+import 'package:taskora/features/auth/presentation/widgets/login_view_widgets/login_view_body_content.dart';
 
 class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
@@ -59,31 +56,14 @@ class _LoginViewBodyState extends State<LoginViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: context.paddingScaffold,
-      child: SafeArea(
-        child: SingleChildScrollView(
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const LoginHeaderSection(),
-                LoginFormSection(
-                  controllerEmail: _controllerEmail,
-                  controllerPassword: _controllerPassword,
-                ),
-                LoginFooterSection(
-                  onLoginPressed: _onLoginPressed,
-                  onCreateAccountTap: _onCreateAccountTap,
-                  rememberMe: _rememberMe,
-                  onRememberMeChanged: _onRememberMeChanged,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return LoginViewBodyContent(
+      formKey: _formKey,
+      controllerEmail: _controllerEmail,
+      controllerPassword: _controllerPassword,
+      rememberMe: _rememberMe,
+      onRememberMeChanged: _onRememberMeChanged,
+      onLoginPressed: _onLoginPressed,
+      onCreateAccountTap: _onCreateAccountTap,
     );
   }
 }
---
+++ lib/features/auth/presentation/widgets/login_view_widgets/login_view_body_content.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:taskora/core/extensions/sizes_extension.dart';
+
+import 'login_footer_section.dart';
+import 'login_form_section.dart';
+import 'login_header_section.dart';
+
+class LoginViewBodyContent extends StatelessWidget {
+  const LoginViewBodyContent({
+    super.key,
+    required this.formKey,
+    required this.controllerEmail,
+    required this.controllerPassword,
+    required this.rememberMe,
+    required this.onRememberMeChanged,
+    required this.onLoginPressed,
+    required this.onCreateAccountTap,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController controllerEmail;
+  final TextEditingController controllerPassword;
+  final bool rememberMe;
+  final ValueChanged<bool> onRememberMeChanged;
+  final VoidCallback onLoginPressed;
+  final VoidCallback onCreateAccountTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: context.paddingScaffold,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const LoginHeaderSection(),
+                LoginFormSection(
+                  controllerEmail: controllerEmail,
+                  controllerPassword: controllerPassword,
+                ),
+                LoginFooterSection(
+                  onLoginPressed: onLoginPressed,
+                  onCreateAccountTap: onCreateAccountTap,
+                  rememberMe: rememberMe,
+                  onRememberMeChanged: onRememberMeChanged,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
