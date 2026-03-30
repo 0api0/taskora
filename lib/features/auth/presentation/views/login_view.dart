c43b903d9a7f4fae61620b4946a62dae845a0188
Mohammed <moeenharb76@gmail.com>
2026-03-30 13:07:36 +0300

    add bloc provider on login view

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:taskora/features/auth/presentation/bloc/auth/auth_bloc.dart';
 
+import '../../../../core/di/service_locator.dart';
 import '../widgets/login_view_widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -7,11 +10,14 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: const Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: LoginViewBody(),
+    return BlocProvider(
+      create: (context) => sl<AuthBloc>(),
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: const Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: LoginViewBody(),
+        ),
       ),
     );
   }
