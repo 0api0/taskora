56ab24cfc31d94922b42f4903721635c8bd05b00
Mohammed <moeenharb76@gmail.com>
2026-04-05 13:19:30 +0300

    Add di in auth_di & splash_onboarding_di.dart

--- lib/core/di/features_di/auth_di.dart
+++ lib/core/di/features_di/auth_di.dart
@@ -1,14 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:taskora/features/auth/data/repositories/auth_session_store_impl.dart';
 import 'package:taskora/features/auth/data/repositories/auth_repository_impl.dart';
 import '../../../features/auth/data/datasources/local/auth_local_data_source.dart';
 import '../../../features/auth/data/datasources/local/auth_local_data_source_impl.dart';
 import '../../../features/auth/data/datasources/remote/auth_remote_data_source.dart';
 import '../../../features/auth/data/datasources/remote/auth_remote_data_source_impl.dart';
 import '../../../features/auth/domain/repositories/auth_repository.dart';
+import '../../../features/auth/domain/repositories/auth_session_manager_store.dart';
 import '../../../features/auth/domain/usecases/check_reset_code_use_case.dart';
 import '../../../features/auth/domain/usecases/forgot_password_use_case.dart';
 import '../../../features/auth/domain/usecases/get_current_user_use_case.dart';
+import '../../../features/auth/domain/usecases/has_session_use_case.dart';
 import '../../../features/auth/domain/usecases/login_use_case.dart';
 import '../../../features/auth/domain/usecases/register_usecase.dart';
 import '../../../features/auth/domain/usecases/reset_password_use_case.dart';
@@ -24,9 +27,14 @@ void setUpAuthDI(GetIt sl) {
     () => AuthLocalDataSourceImpl(sl<SharedPrefService>()),
   );
 
+  // -- auth session store --
+  sl.registerLazySingleton<AuthSessionStore>(
+    () => AuthSessionStoreImpl(sl<AuthLocalDataSource>()),
+  );
+
   // -- auth interceptor --
   sl.registerLazySingleton<AuthInterceptor>(
-    () => AuthInterceptor(sl<AuthLocalDataSource>()),
+    () => AuthInterceptor(sl<AuthSessionStore>(), sl<AuthLocalDataSource>()),
   );
 
   if (!sl<Dio>().interceptors.any(
@@ -45,6 +53,7 @@ void setUpAuthDI(GetIt sl) {
     () => AuthRepositoryImpl(
       authRemoteDataSource: sl<AuthRemoteDataSource>(),
       authLocalDataSource: sl<AuthLocalDataSource>(),
+      authSessionStore: sl<AuthSessionStore>(),
     ),
   );
 
@@ -72,6 +81,10 @@ void setUpAuthDI(GetIt sl) {
   sl.registerLazySingleton<GetCurrentUserUseCase>(
     () => GetCurrentUserUseCase(sl<AuthRepository>()),
   );
+  sl.registerLazySingleton<HasSessionUseCase>(
+    () => HasSessionUseCase(sl<AuthSessionStore>()),
+  );
+
   // -- blocs --
   sl.registerFactory<AuthBloc>(
     () => AuthBloc(
--- lib/core/di/features_di/splash_onboarding_di.dart
+++ lib/core/di/features_di/splash_onboarding_di.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 
+import '../../../features/auth/domain/usecases/has_session_use_case.dart';
 import '../../../features/splash_onboarding/data/datasources/local/onboarding_local_data_source.dart';
 import '../../../features/splash_onboarding/data/datasources/local/onboarding_local_data_source_impl.dart';
 import '../../../features/splash_onboarding/data/repositories/onboarding_repository_impl.dart';
@@ -7,6 +8,7 @@ import '../../../features/splash_onboarding/domain/repositories/onboarding_repos
 import '../../../features/splash_onboarding/domain/usecases/check_onboarding_status_usecase.dart';
 import '../../../features/splash_onboarding/domain/usecases/complete_onboarding_usecase.dart';
 import '../../../features/splash_onboarding/domain/usecases/get_onboarding_pages_usecase.dart';
+import '../../../features/splash_onboarding/domain/usecases/resolve_startup_destination_use_case.dart';
 import '../../../features/splash_onboarding/presentation/bloc/splash_onboarding_bloc.dart';
 import '../../services/local/shared_pref_service.dart';
 
@@ -33,11 +35,17 @@ void setupSplashOnboardingDI(GetIt sl) {
   sl.registerLazySingleton<GetOnboardingPagesUseCase>(
     () => const GetOnboardingPagesUseCase(),
   );
+  sl.registerLazySingleton<ResolveStartupDestinationUseCase>(
+    () => ResolveStartupDestinationUseCase(
+      sl<CheckOnboardingStatusUseCase>(),
+      sl<HasSessionUseCase>(),
+    ),
+  );
 
   // -- bloc --
   sl.registerFactory<SplashOnboardingBloc>(
     () => SplashOnboardingBloc(
-      sl<CheckOnboardingStatusUseCase>(),
+      sl<ResolveStartupDestinationUseCase>(),
       sl<CompleteOnboardingUseCase>(),
     ),
   );
