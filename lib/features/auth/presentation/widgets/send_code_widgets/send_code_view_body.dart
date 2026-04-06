7bf76259b09540246207628d09c21b8a3408cbee
Mohammed <moeenharb76@gmail.com>
2026-04-06 14:20:51 +0300

    Add bloc consumer on send code view body

--- lib/features/auth/presentation/widgets/send_code_widgets/send_code_view_body.dart
+++ lib/features/auth/presentation/widgets/send_code_widgets/send_code_view_body.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:taskora/core/router/routers_name.dart';
+import 'package:taskora/features/auth/params/check_reset_code_params.dart';
+import 'package:taskora/features/auth/presentation/bloc/password_recovery/password_recovery_bloc.dart';
+import 'package:taskora/features/auth/presentation/bloc/password_recovery/password_recovery_event.dart';
+import 'package:taskora/features/auth/presentation/bloc/password_recovery/password_recovery_state.dart';
 import 'package:taskora/features/auth/presentation/widgets/send_code_widgets/send_code_view_body_content.dart';
 
 class SendCodeViewBody extends StatefulWidget {
@@ -32,15 +37,48 @@ class _SendCodeViewBodyState extends State<SendCodeViewBody> {
       return;
     }
     final pinCode = _controllerPinCode.text.trim();
-    context.push(RoutersName.authRoute.newPassword);
-    // TODO: Bloc
+    final pinCodeInt = int.tryParse(pinCode) ?? 0;
+    context.read<PasswordRecoveryBloc>().add(
+      CheckResetCodeRequested(CheckResetCodeParams(code: pinCodeInt)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return SendCodeViewBodyContent(
-      isLoading:, controllerPinCode: _controllerPinCode,
-      formKey: _formKey,
-      onVerifyCodePressed: _onVerifyCodePressed,);
+    return BlocConsumer<PasswordRecoveryBloc, PasswordRecoveryState>(
+      listener: (context, state) {
+        debugPrint('status: ${state.status}');
+        debugPrint(
+          'codeVerificationEntity: ${state.forgotPasswordVerification}',
+        );
+        debugPrint('message: ${state.message}');
+        if ((state.status == PasswordRecoveryStatus.failure ||
+                state.status == PasswordRecoveryStatus.networkFailure) &&
+            state.message != null &&
+            state.message!.isNotEmpty) {
+          context.push(
+            RoutersName.authRoute.bodyError,
+            extra: {
+              'message': state.message,
+              'passwordRecoveryStatus': state.status,
+            },
+          );
+        }
+
+        if (state.status == PasswordRecoveryStatus.success &&
+            state.codeVerification != null) {
+          context.push(RoutersName.authRoute.newPassword);
+        }
+      },
+      builder: (context, state) {
+        final isLoading = state.status == PasswordRecoveryStatus.loading;
+        return SendCodeViewBodyContent(
+          isLoading: isLoading,
+          controllerPinCode: _controllerPinCode,
+          formKey: _formKey,
+          onVerifyCodePressed: isLoading ? null : _onVerifyCodePressed,
+        );
+      },
+    );
   }
 }
