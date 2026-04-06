15d05b1357b9765046fc28d42da7a33bf98cdcc1
Mohammed <moeenharb76@gmail.com>
2026-04-06 13:18:18 +0300

    Add bloc provider on send code view

--- lib/features/auth/presentation/views/send_code_view.dart
+++ lib/features/auth/presentation/views/send_code_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:taskora/features/auth/presentation/bloc/password_recovery/password_recovery_bloc.dart';
 
+import '../../../../core/di/service_locator.dart';
 import '../widgets/send_code_widgets/send_code_view_body.dart';
 
 class SendCodeView extends StatelessWidget {
@@ -7,11 +10,14 @@ class SendCodeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: const Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: SendCodeViewBody(),
+    return BlocProvider(
+      create: (context) => sl<PasswordRecoveryBloc>(),
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: const Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: SendCodeViewBody(),
+        ),
       ),
     );
   }
