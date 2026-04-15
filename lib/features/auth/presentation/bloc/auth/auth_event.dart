15aae848e25fbe61f1618c8377869b9c5eb0b1bb
Mohammed <moeenharb76@gmail.com>
2026-04-15 13:03:07 +0300

    add equatable on auth & password recovery (event)

--- lib/features/auth/presentation/bloc/auth/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth/auth_event.dart
@@ -1,23 +1,33 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
 import '../../../params/login_params.dart';
 import '../../../params/register_params.dart';
 
 @immutable
-sealed class AuthEvent {
+sealed class AuthEvent extends Equatable {
   const AuthEvent();
+
+  @override
+  List<Object?> get props => const [];
 }
 
 final class LoginRequested extends AuthEvent {
   const LoginRequested(this.params);
 
   final LoginParams params;
+
+  @override
+  List<Object?> get props => [params];
 }
 
 final class RegisterRequested extends AuthEvent {
   const RegisterRequested(this.params);
 
   final RegisterParams params;
+
+  @override
+  List<Object?> get props => [params];
 }
 
 final class GetCurrentUserRequested extends AuthEvent {
--- lib/features/auth/presentation/bloc/password_recovery/password_recovery_event.dart
+++ lib/features/auth/presentation/bloc/password_recovery/password_recovery_event.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
 import '../../../params/check_reset_code_params.dart';
@@ -5,26 +6,34 @@ import '../../../params/forgot_password_params.dart';
 import '../../../params/reset_password_params.dart';
 
 @immutable
-sealed class PasswordRecoveryEvent {
+sealed class PasswordRecoveryEvent extends Equatable {
   const PasswordRecoveryEvent();
+  @override
+  List<Object?> get props => const [];
 }
 
 final class ForgotPasswordRequested extends PasswordRecoveryEvent {
   const ForgotPasswordRequested(this.params);
 
   final ForgotPasswordParams params;
+  @override
+  List<Object?> get props => [params];
 }
 
 final class CheckResetCodeRequested extends PasswordRecoveryEvent {
   const CheckResetCodeRequested(this.params);
 
   final CheckResetCodeParams params;
+  @override
+  List<Object?> get props => [params];
 }
 
 final class ResetPasswordRequested extends PasswordRecoveryEvent {
   const ResetPasswordRequested(this.params);
 
   final ResetPasswordParams params;
+  @override
+  List<Object?> get props => [params];
 }
 
 final class ResetPasswordRecoveryStateRequested extends PasswordRecoveryEvent {
